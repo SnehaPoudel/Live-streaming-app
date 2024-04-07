76d38c287f4d8fc9cbfa71ef69e229fb9350382d
Sneha Poudel <sneha.poudel@deerwalk.edu.np>
2024-04-07 11:33:59 +0545

    Created Clerk WebHooks

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: ["/api/webhooks(.*)"], // Add the route to your API end
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
