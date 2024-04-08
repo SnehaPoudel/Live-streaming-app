d70233865766330c505661d3c59b7d07c3b25a75
Sneha Poudel <sneha.poudel@deerwalk.edu.np>
2024-04-08 21:58:56 +0545

    Follow service

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,3 +1,8 @@
+import { isFollowingUser } from "@/lib/follow-service";
+import { getUserByUsername } from "@/lib/user-service";
+import { notFound } from "next/navigation";
+import { Actions } from "./_components/actions";
+
 interface UserPageProps {
     params: {
       username: string;
@@ -7,10 +12,19 @@ interface UserPageProps {
 const UserPage = async ({
     params
 }: UserPageProps) => {
-    
+    const user = await getUserByUsername(params.username);
+    if (!user) {
+      notFound();
+    }
+
+    const isFollowing = await isFollowingUser(user.id);
+   
     return ( 
-      <div>
-        User: {params.username} 
+      <div className="flex flex-col gap-y-4">
+        <p>username: {user.username}</p>
+        <p>user ID: {user.id}</p>
+        <p>is following: {`${isFollowing}`}</p>
+        <Actions userId={user.id} isFollowing = {isFollowing}/>  
       </div>
     );
   }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider, authMiddleware } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import {Toaster} from 'sonner';
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
@@ -21,11 +22,13 @@ export default function RootLayout({
     <ClerkProvider appearance={{baseTheme:dark}}>
       <html lang="en">
         <body className={inter.className}>
+          
           <ThemeProvider
             attribute="class"
             forcedTheme="dark"
             storageKey="livestream-theme"
           >
+            <Toaster theme="light" position="bottom-center"/>  
             {children}
           </ThemeProvider>
           </body>
