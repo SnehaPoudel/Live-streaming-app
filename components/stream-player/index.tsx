584648795db49624f8def0feab5c34636f86a13b
Sneha Poudel <sneha.poudel@deerwalk.edu.np>
2024-04-14 15:27:25 +0545

    About Card

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -10,9 +10,13 @@ import { Chat } from "./chat";
 import { ChatToggle } from "./chat-toggle";
 import { Header } from "./header";
 import { InfoCard } from "./info-card";
+import { AboutCard } from "./about-card";
 
 interface StreamPlayerProps {
-    user: User & { stream : Stream | null};
+    user: User & { 
+        stream : Stream | null, 
+        _count: { followedBy: number}
+    };
     stream: Stream;
     isFollowing: boolean;
 }
@@ -72,13 +76,13 @@ export const StreamPlayer = ({
                     name={stream.name}
                     thumbnailUrl={stream.thumbnailUrl}
                     />
-                    {/* <AboutCard
+                    <AboutCard
                     hostName={user.username}
                     hostIdentity={user.id}
                     viewerIdentity={identity}
                     bio={user.bio}
                     followedByCount={user._count.followedBy}
-                    /> */}
+                    />
 
                 </div>
                 <div
